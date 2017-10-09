90925aba3afe825d40d8a2d9c313c71cf29637fa
Maciej Modliński <maciej.modlinski@veneo.eu>
2017-10-09 16:37:08 +0200

    zadanie 1

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+import Events from './events/Events';
+
+const App = () => {
+    return(
+        <Router>
+            <div>
+                <Route path="/" component={Events} />
+            </div>
+        </Router>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Events from './Events';
+import App from './app';
 
-ReactDOM.render(<Events />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
